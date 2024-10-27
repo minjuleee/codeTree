8b04856f9189263bc53c733fbbd715c9e4289c30
minjuleee <117346051+minjuleee@users.noreply.github.com>
2024-10-27 19:54:25 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241027/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.c
+++ 241027/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.c
@@ -4,30 +4,29 @@
 char input[100];
 char target[100];
 
-int findSubstringIndex(){
-
+int findSubstringIndex() {
     int inputLen = strlen(input);
     int targetLen = strlen(target);
 
-    for(int i=0; i <= inputLen - targetLen; i++){
-        if(strncmp(&input[i], target, targetLen) == 0){
+    for (int i = 0; i <= inputLen - targetLen; i++) {
+        if (strncmp(&input[i], target, targetLen) == 0) {
             return i;
         }
     }
-    
     return -1;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 사용자로부터 입력받기
     fgets(input, sizeof(input), stdin);
     fgets(target, sizeof(target), stdin);
 
+    // 입력된 문자열에서 개행 문자 제거
     input[strcspn(input, "\n")] = 0;
-    input[strcspn(target, "\n")] = 0;
+    target[strcspn(target, "\n")] = 0;
 
     int index = findSubstringIndex();
-    printf("%d", index);
+    printf("%d\n", index); // 인덱스 출력
 
     return 0;
 }
